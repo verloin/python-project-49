57c606a8771f6482a378bf8ff53996df223b31fc
y.matytsin <y.matytsin@vk.team>
2026-04-04 17:48:41 +0300

    step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,13 @@
 from brain_games.cli import welcome_user
 
+
 def func():
     print('Welcome to the Brain Games!')
-    
-    
-    
+
 
 def main():
     func()
     welcome_user()
-    
-    
+
 
 main()
