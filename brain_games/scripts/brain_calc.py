8980cd707f40ecfc629219be0771e4f159ed607f
y.matytsin <y.matytsin@vk.team>
2026-04-05 13:38:24 +0300

    step 6 calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,37 @@
+import random
+import prompt
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    
+    print('What is the result of the expression?')
+    count = 0
+    while count < 3:
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        options = ['-', '+', '*']
+        op = random.choice(options)
+        
+        result = eval(f"{a} {op} {b}")
+        
+        print(f'Question: {a} {op} {b}')
+        answer = input('Your answer: ')
+
+        if int(answer) == result:
+            count += 1
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
+            print(f"Let's try again, {name}!")
+            return
+        
+        if count == 3:
+            print(f'Congratulations, {name}!')
+            return
+    
+    
+if __name__ == "__main__":
+    main()
