a13bffc4c028fee9c9a668179b59abbed6ad9c71
y.matytsin <y.matytsin@vk.team>
2026-04-05 16:15:47 +0300

    check 01

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,8 @@
 from brain_games.cli import welcome_user
 
 
-def func():
-    print('Welcome to the Brain Games!')
-
-
 def main():
-    func()
+    welcome_user()
 
-
-main()
+if __name__ == "__main__":
+    main()
